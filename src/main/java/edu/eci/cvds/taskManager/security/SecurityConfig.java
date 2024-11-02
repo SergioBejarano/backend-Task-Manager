6cc9eb0e37bfb150528de37eb375bc1ac396b5ab
LauraRo166 <laura.rsanchez@mail.escuelaing.edu.co>
2024-11-01 23:40:12 -0500

    Retorno antes de cambios para cors

--- src/main/java/edu/eci/cvds/taskManager/security/SecurityConfig.java
+++ src/main/java/edu/eci/cvds/taskManager/security/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
         return authProvider;
     }
 
+
     /**
      * Provides an AuthenticationManager bean using the provided AuthenticationConfiguration.
      *
